1f3f66f177702ab92dd0d8655e32e862c2e8e838
Andrew Williams <1williamsaj@gmail.com>
2019-09-02 15:01:55 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,5 +3,5 @@ def prime?(num)
 (2..num-1).none? {|i| num%i == 0} && num > 1 # We use an 'and' operator to also check to see if the 'num' is greater than 1.
                  # ^ This section of code iterates through our range of numbers and checks to see if the inputted value is divisible.
                  # Since the range starts at 2 and only goes up to the 'num' minus 1, this function will never divide the num by 1 or itself.
-                 # So ultimately, our function will check the range using 'none' to make sure
+                 # So ultimately, our function will check the range using 'none' to make sure the inputted 'num' is not divisible by any number in the range.
 end
